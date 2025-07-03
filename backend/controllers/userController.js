96ae09dd90fea770e2b431734266c4443bd3de4a
AaravT17 <aarav.tandon.004@gmail.com>
2025-07-04 02:08:09 +0530

    wrapped route handlers in asyncHandler to account for async work to be done when interacting with the database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,33 +1,39 @@
+import asyncHandler from 'express-async-handler'
+/* When interacting with the database, each of the route handlers will
+ * become async, and wrapping them in the asyncHandler allows us to avoid
+ * having to write try/catch blocks for error handling in each function
+ */
+
 // @desc Login user
 // @route GET /api/users
 // @access Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({message: 'Login user'})
-}
+})
 
 // @desc Register user
 // @route POST /api/users
 // @access Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   if (!req.body.name) {
     res.status(400)
     throw new Error("Please fill in the name field")
   }
   res.json({message: 'Register user'})
-}
+})
 
 // @desc Update user
 // @route PUT /api/users/:id
 // @access Private
-const updateUser = (req, res) => {
+const updateUser = asyncHandler(async (req, res) => {
   res.json({message: `Update user ${req.params.id}`})
-}
+})
 
 // @desc Delete user
 // @route DELETE /api/users/:id
 // @access Private
-const deleteUser = (req, res) => {
+const deleteUser = asyncHandler(async (req, res) => {
   res.json({message: `Delete user ${req.params.id}`})
-}
+})
 
 export { loginUser, registerUser, updateUser, deleteUser }
