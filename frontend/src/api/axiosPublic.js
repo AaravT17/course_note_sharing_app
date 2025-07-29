c45109f3409a96e5456d51f7ae7a2478f101deff
AaravT17 <aarav.tandon.004@gmail.com>
2025-07-29 22:14:08 +0530

    fixed typo in name of env variable used in axiosPublic base url

--- frontend/src/api/axiosPublic.js
+++ frontend/src/api/axiosPublic.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosPublic = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || '',
+  baseURL: import.meta.env.VITE_BACKEND_BASE_URL || '',
 })
 
 export default axiosPublic
