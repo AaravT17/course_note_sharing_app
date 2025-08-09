92adf1b90a10e6bc9b49ee8e0ed8b5d5cd00c5db
AaravT17 <aarav.tandon.004@gmail.com>
2025-08-09 19:44:09 +0530

    changed max notes per search to 20 from 30

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -16,6 +16,6 @@ export const MAX_RECENT_NOTES = 20
 
 export const MAX_LIKED_NOTES_DASHBOARD = 20
 
-export const MAX_NOTES_PER_SEARCH = 30
+export const MAX_NOTES_PER_SEARCH = 20
 
 export const RANDOM_TOKEN_LENGTH_BYTES = 32
